03571c647a3720f77bd690dad36eec20f9ca1b3c
Imad-Chakir <cimad1812@gmail.com>
2023-04-26 11:58:50 +0200

    test commit

--- src/main/java/com/example/teamshare/TeamShareApplication.java
+++ src/main/java/com/example/teamshare/TeamShareApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TeamShareApplication {
 
+    //    mini change
     public static void main(String[] args) {
         SpringApplication.run(TeamShareApplication.class, args);
     }
